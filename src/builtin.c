1eff7b772954231b908ecb6b168e31688e4239b0
Matias Rozanec <rozanecm@gmail.com>
2018-04-21 13:01:35 -0300

    comlete cd builtin command

--- src/builtin.c
+++ src/builtin.c
@@ -1,12 +1,26 @@
 #include "builtin.h"
 
+char* split_by_space(char* cmd){
+    int i = 0;
+    
+    while(cmd[i] != ' ')
+        ++i;
+
+    cmd[i++] = '\0';
+    
+    while(cmd[i] == ' ')
+        ++i;
+
+    return &cmd[i];
+}
+
 // returns true if the 'exit' call
 // should be performed
 int exit_shell(char* cmd) {
 
-	// Your code here 
+    // Your code here
 
-	return 0;
+    return 0;
 }
 
 // returns true if "chdir" was performed
@@ -15,18 +29,34 @@ int exit_shell(char* cmd) {
 // 	$ cd (change to HOME)
 // it has to be executed and then return true
 int cd(char* cmd) {
-	
-	// Your code here
+    /* lets assume no command name will take more than 100 bytes */
+    char cmd_name[100];
+    sscanf(cmd, "%s", cmd_name);
 
-	return 0;
+    if(strcmp(cmd_name, "cd") == 0) {
+        char* args = split_by_space(cmd);
+
+        if (strlen(args) > 0) {
+            chdir(args);
+        } else {
+            chdir(getenv("HOME"));
+        }
+
+        char* current_path = get_current_dir_name();
+        snprintf(promt, sizeof promt, "(%s)", current_path);
+        free(current_path);
+        return 1;
+    }
+    
+    return 0;
 }
 
 // returns true if 'pwd' was invoked
 // in the command line
 int pwd(char* cmd) {
 
-	// Your code here
-	
-	return 0;
+    // Your code here
+
+    return 0;
 }
 
