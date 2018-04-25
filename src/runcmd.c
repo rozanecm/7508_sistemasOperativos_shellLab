a8c0cb36a13a2ec09e8625668f3ffd9308a18fbd
Matias Rozanec <rozanecm@gmail.com>
2018-04-25 12:27:37 -0300

    retab file

--- src/runcmd.c
+++ src/runcmd.c
@@ -5,63 +5,63 @@ struct cmd* parsed_pipe;
 
 // runs the command in 'cmd'
 int run_cmd(char* cmd) {
-	
-	pid_t p;
-	struct cmd *parsed;
+    
+    pid_t p;
+    struct cmd *parsed;
 
-	// if the "enter" key is pressed
-	// just print the promt again
-	if (cmd[0] == END_STRING)
-		return 0;
+    // if the "enter" key is pressed
+    // just print the promt again
+    if (cmd[0] == END_STRING)
+        return 0;
 
-	// cd built-in call
-	if (cd(cmd))
-		return 0;
+    // cd built-in call
+    if (cd(cmd))
+        return 0;
 
-	// exit built-in call
-	if (exit_shell(cmd))
-		return EXIT_SHELL;
+    // exit built-in call
+    if (exit_shell(cmd))
+        return EXIT_SHELL;
 
-	// pwd buil-in call
-	if (pwd(cmd))
-		return 0;
+    // pwd buil-in call
+    if (pwd(cmd))
+        return 0;
 
-	// parses the command line
-	parsed = parse_line(cmd);
-	
-	// forks and run the command
-	if ((p = fork()) == 0) {
-		
-		// keep a reference
-		// to the parsed pipe cmd
-		// so it can be freed later
-		if (parsed->type == PIPE)
-			parsed_pipe = parsed;
+    // parses the command line
+    parsed = parse_line(cmd);
+    
+    // forks and run the command
+    if ((p = fork()) == 0) {
+        
+        // keep a reference
+        // to the parsed pipe cmd
+        // so it can be freed later
+        if (parsed->type == PIPE)
+            parsed_pipe = parsed;
 
-		exec_cmd(parsed);
-	}
+        exec_cmd(parsed);
+    }
 
-	// store the pid of the process
-	parsed->pid = p;
+    // store the pid of the process
+    parsed->pid = p;
 
-	// background process special treatment
-	// Hint:
-	// - check if the process is 
-	// 	going to be run in the 'back'
-	// - print info about it with 
-	// 	'print_back_info()'
-	//
-	// Your code here
+    // background process special treatment
+    // Hint:
+    // - check if the process is 
+    //  going to be run in the 'back'
+    // - print info about it with 
+    //  'print_back_info()'
+    //
+    // Your code here
     if(parsed->type == BACK)
         return 0;
 
-	// waits for the process to finish
-	waitpid(p, &status, 0);
-	
-	print_status_info(parsed);
-	
-	free_command(parsed);
+    // waits for the process to finish
+    waitpid(p, &status, 0);
+    
+    print_status_info(parsed);
+    
+    free_command(parsed);
 
-	return 0;
+    return 0;
 }
 
